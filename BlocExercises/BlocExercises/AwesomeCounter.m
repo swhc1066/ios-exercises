e8da6a9b44eba42a06e221eb3284ad4ee06f227b
Sean Craig <fwcycling@gmail.com>
2015-08-03 13:41:50 -0500

    Added notes to solution for Assignement 12 loops

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -13,21 +13,34 @@
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE*/
     
+    
+    //initialize variable
     NSInteger i = number;
     NSInteger j = otherNumber;
+    
+    //empty string - will be the string we return to verify the test works
     NSString *returnString = [NSString string];
     
+    //if statement to determine which function to run since we have 3 conditions for test based on which number is low
     if (number < otherNumber) {
+        
+    //for statement to incrementally add number in string
         for (i = number; i <= otherNumber; i++) {
             NSString *blankString = @"";
+            //changes int to a string format
             NSString *newString = [returnString stringByAppendingFormat:@"%li", (long)i];
+            //adds newString to returnString
             returnString = [newString stringByAppendingString:blankString];
             NSLog(@"%@", returnString);
         }
+        //runs else statement since number is greater then otherNumber
     } else {
+        //for statement to incrementally add number in string
         for (j = otherNumber; j <= number; j++) {
             NSString *blankString = @"";
+            //changes int to a string format
             NSString *newString = [returnString stringByAppendingFormat:@"%li", (long)j];
+             //adds newString to returnString
             returnString = [newString stringByAppendingString:blankString];
             NSLog(@"%@", returnString);
         }
