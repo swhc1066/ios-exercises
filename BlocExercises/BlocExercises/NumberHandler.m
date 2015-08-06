e24adbae70be4742474ac68ea914dfc7b1dcd4c0
Sean Craig <fwcycling@gmail.com>
2015-08-06 15:37:45 -0500

    Assignment Objects complete

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,38 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+
+    int newNumber = [number intValue];
+    newNumber = newNumber * 2;
+    number = [NSNumber numberWithInt:newNumber];
+    
+    return number;
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @[];
+    NSMutableArray *returnArray = [NSMutableArray array];
+    
+    while (number <= otherNumber) {
+        NSNumber *newNumber = [NSNumber numberWithInteger:number];
+        [returnArray addObject:newNumber];
+        number++;
+    }
+
+    return returnArray;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
-    return 0;
+    
+    NSMutableArray *numbersArray = [arrayOfNumbers mutableCopy];
+    NSSortDescriptor *sortNumbers = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES];
+    [numbersArray sortUsingDescriptors:@[sortNumbers]];
+    
+    NSInteger myInt = [[numbersArray objectAtIndex:0] intValue];
+
+    
+    return myInt;
 }
 
 @end
